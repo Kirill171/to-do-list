e15c824dce834142a05c09ac696c7bf04aafcfae
Kirill <kiria12312@mail.ru>
2024-04-08 05:26:00 +0300

    Final commit

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
 class ToDo {
+    static counter = 0;
+
     constructor(text) {
         this.text = text;
     }
 
-    addTask(){
+    addTask() {
+        ToDo.counter++;
+        let task = 'task' + ' ' + ToDo.counter;
         let toDoList = document.querySelector(".list");
         let li = document.createElement('li');
-        // text.textContent = this.text;
-        
+        li.addEventListener('click', (event) => {
+            event.stopPropagation();
+            li.classList.toggle('checked');
+            checkbox.checked = !checkbox.checked;
+        })
+
         let checkbox = document.createElement("input");
         checkbox.type = 'checkbox';
-        checkbox.id = 'task';
-        
+        checkbox.id = task;
+        checkbox.addEventListener('change', () => {
+            if (checkbox.checked) {
+                checkbox.checked = false;
+            } else {
+                checkbox.checked = true;
+            }
+        });
+
         let label = document.createElement("label");
         label.textContent = this.text;
-        label.setAttribute('for', 'task');
+        label.setAttribute('for', task);
+        label.addEventListener('click', (event) => {
+            event.stopPropagation();
+        })
+
+        let btn = document.createElement("button");
+        btn.className = 'deleteTaskBtn';
+        btn.textContent = 'X';
+        btn.addEventListener("click", deleteTaskHandler);
 
         li.append(checkbox);
         li.append(label);
-        // <input type="checkbox" name="str" id="str">
-        // <label for="str">teasda</label>
+        li.append(btn);
         toDoList.append(li);
     }
 }
 
-let btn = document.querySelector(".button");
+let addTaskBtn = document.querySelector(".addTaskBtn");
 let startMassage = document.querySelector(".start-massage");
-let inputField  = document.querySelector("#to-do");
+let inputField = document.querySelector("#to-do");
+let deleteTaskBtn = document.querySelectorAll(".deleteTaskBtn");
 
-btn.addEventListener('click', addTaskHandler);
+addTaskBtn.addEventListener('click', addTaskHandler);
 
-inputField.addEventListener('keydown', (e) => {
-    if(e.code == 'Enter') addTaskHandler();
+inputField.addEventListener('keydown', (e) => { // Добавление задач на Enter
+    if (e.code == 'Enter') addTaskHandler();
 });
 
-function addTaskHandler() {
+function addTaskHandler() { // Кнопка добавления задач
     let text = inputField.value;
     if (text) {
-        if(!startMassage.hidden) startMassage.hidden = true;
+        if (!startMassage.hidden) startMassage.hidden = true;
 
         let toDo = new ToDo(text);
         toDo.addTask();
@@ -45,4 +68,10 @@ function addTaskHandler() {
     } else {
         alert("Сначала введите текст задачи.");
     }
+}
+
+function deleteTaskHandler(e) { // Кнопка удаления задач
+    this.parentNode.remove();
+    ToDo.counter--;
+    if (ToDo.counter == 0) startMassage.hidden = false;
 }
